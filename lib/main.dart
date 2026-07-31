e9bec18342949d8a4603bf4a726502a1b27a504c
NolanLefebvre <nolan.lefebvre69@gmail.com>
2026-07-31 23:16:59 +0200

    affichage et ajout de livres fonctionel

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   runApp(const MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
